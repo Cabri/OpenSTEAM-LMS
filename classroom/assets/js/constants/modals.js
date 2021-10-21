17de1390669ace50123af4c7d4bd842869b6b1ab
seif <seif.benomar@cabri.com>
2021-10-21 10:31:59 +0200

    [CHG] update activities limitation message

--- classroom/assets/js/constants/modals.js
+++ classroom/assets/js/constants/modals.js
@@ -900,7 +900,7 @@ const classroomModals = {
             </div>
 
           <div class="row  justify-content-md-center"">
-                <button class="save-student-in-classroom c-btn-primary m-3" onclick="startActivityCreation()"><i class="fas fa-save"></i> <span class="ml-1" data-i18n="classroom.modals.addActivity.continue"></span></button>
+                <button class="c-btn-primary m-3" onclick="startActivityCreation()"><i class="fas fa-save"></i> <span class="ml-1" data-i18n="classroom.modals.addActivity.continue"></span></button>
           </div>
       `,
       footer: ``
@@ -918,7 +918,7 @@ const classroomModals = {
             </div>
 
           <div class="row  justify-content-md-center"">
-                <button class="save-student-in-classroom c-btn-primary m-3" onclick="window.pseudoModal.closeModal('add-activity-limitation')"><i class="fas fa-check"></i> <span class="ml-1">Ok</span></button>
+                <button class="c-btn-primary m-3" onclick="window.pseudoModal.closeModal('add-activity-limitation')"><i class="fas fa-check"></i> <span class="ml-1">Ok</span></button>
           </div>
       `,
     footer: ``
